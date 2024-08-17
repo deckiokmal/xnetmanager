edfc49c0fe517bd1e96b85121ce20212749347af
deckiokmal <dopentindo@gmail.com>
2024-08-17 17:31:36 +0700

    update openai validate function description

--- src/utils/openai_utils.py
+++ src/utils/openai_utils.py
@@ -1,6 +1,68 @@
 import openai
 
 
+template_function_description = [
+    {
+        "name": "generate_device_configuration_template",
+        "description": "Generate a Jinja2 template with YAML parameters for configuring network devices like Fortinet. The response should only contain the template with no extra text to avoid errors when pushing the configuration to the devices.",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "device_type": {
+                    "type": "string",
+                    "description": "Type of network device (e.g., Fortinet).",
+                },
+                "hostname": {
+                    "type": "string",
+                    "description": "Hostname of the network device.",
+                },
+                "interfaces": {
+                    "type": "array",
+                    "items": {
+                        "type": "object",
+                        "properties": {
+                            "name": {
+                                "type": "string",
+                                "description": "Interface name.",
+                            },
+                            "ip_address": {
+                                "type": "string",
+                                "description": "IP address of the interface.",
+                            },
+                            "subnet_mask": {
+                                "type": "string",
+                                "description": "Subnet mask of the interface.",
+                            },
+                        },
+                        "required": ["name", "ip_address", "subnet_mask"],
+                    },
+                    "description": "List of interfaces to configure.",
+                },
+                "routes": {
+                    "type": "array",
+                    "items": {
+                        "type": "object",
+                        "properties": {
+                            "destination": {
+                                "type": "string",
+                                "description": "Destination network.",
+                            },
+                            "gateway": {
+                                "type": "string",
+                                "description": "Gateway IP address for the route.",
+                            },
+                        },
+                        "required": ["destination", "gateway"],
+                    },
+                    "description": "List of static routes.",
+                },
+            },
+            "required": ["device_type", "hostname", "interfaces"],
+        },
+    }
+]
+
+
 def validate_generated_template_with_openai(config, vendor):
     response = openai.ChatCompletion.create(
         model="gpt-4o-mini",
