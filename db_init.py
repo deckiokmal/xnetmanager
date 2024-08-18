ffa78c1cbf687095e16154b0a21ab2f5ee5ef528
deckiokmal <dopnetindo@gmail.com>
2024-08-18 12:37:26 +0700

    tambah table backup dan revision serta relasi baru

--- db_init.py
+++ db_init.py
@@ -1,81 +1,90 @@
 from src import db, create_app
-from src.models.app_models import User, Role, Permission, UserRoles, RolePermissions
+from src.models.app_models import (
+    User,
+    Role,
+    Permission,
+    UserRoles,
+    RolePermissions,
+)  # Pastikan semua model terimport
 
 def initialize():
     app = create_app()
     with app.app_context():
-        # Buat database dan tabel-tabelnya jika belum ada
-        db.create_all()
+        try:
+            # Buat database dan tabel-tabelnya jika belum ada
+            db.create_all()
 
-        # Buat peran dan izin default
-        roles_permissions = {
-            "Admin": [
-                "Manage Roles",
-                "Manage Users",
-                "Manage Devices",
-                "Manage Templates",
-                "Manage Config",
-                "View Devices",
-                "View Templates",
-                "Manage Profile",
-            ],
-            "User": [
-                "Manage Devices",
-                "Manage Templates",
-                "Manage Config",
-                "View Devices",
-                "View Templates",
-                "Manage Profile",
-            ],
-            "View": ["View Devices", "View Templates", "Manage Profile"],
-        }
+            # Buat peran dan izin default
+            roles_permissions = {
+                "Admin": [
+                    "Manage Roles",
+                    "Manage Users",
+                    "Manage Devices",
+                    "Manage Templates",
+                    "Manage Config",
+                    "View Devices",
+                    "View Templates",
+                    "Manage Profile",
+                ],
+                "User": [
+                    "Manage Devices",
+                    "Manage Templates",
+                    "Manage Config",
+                    "View Devices",
+                    "View Templates",
+                    "Manage Profile",
+                ],
+                "View": ["View Devices", "View Templates", "Manage Profile"],
+            }
 
-        # Tambahkan role dan permission ke dalam database
-        for role_name, permissions in roles_permissions.items():
-            role = Role.query.filter_by(name=role_name).first()
-            if not role:
-                role = Role(name=role_name)
-                db.session.add(role)
-                db.session.commit()
-
-            for perm_name in permissions:
-                permission = Permission.query.filter_by(name=perm_name).first()
-                if not permission:
-                    permission = Permission(
-                        name=perm_name, description=f"Dapat {perm_name.lower()}"
-                    )
-                    db.session.add(permission)
+            # Tambahkan role dan permission ke dalam database
+            for role_name, permissions in roles_permissions.items():
+                role = Role.query.filter_by(name=role_name).first()
+                if not role:
+                    role = Role(name=role_name)
+                    db.session.add(role)
                     db.session.commit()
 
-                if permission not in role.permissions:
-                    role.permissions.append(permission)
+                for perm_name in permissions:
+                    permission = Permission.query.filter_by(name=perm_name).first()
+                    if not permission:
+                        permission = Permission(
+                            name=perm_name, description=f"Permission to {perm_name.lower()}"
+                        )
+                        db.session.add(permission)
+                        db.session.commit()
 
-            db.session.commit()
+                    if permission not in role.permissions:
+                        role.permissions.append(permission)
 
-        # Buat pengguna default dengan peran Admin
-        default_email = "xnetmanager@example.com"
-        default_password = (
-            "xnetmanager"  # Jangan gunakan password default seperti ini di produksi
-        )
+                db.session.commit()
 
-        if not User.query.filter_by(email=default_email).first():
-            admin_role = Role.query.filter_by(name="Admin").first()
-            # Tambahkan first_name dan last_name di sini
-            admin_user = User(
-                first_name="Xnet",
-                last_name="Manager",
-                email=default_email,
-                password_hash=default_password
-            )
-            # Atur atribut tambahan
-            admin_user.roles.append(admin_role)
-            db.session.add(admin_user)
-            db.session.commit()
+            # Buat pengguna default dengan peran Admin
+            default_email = "xnetmanager@example.com"
+            default_password = "xnetmanager"  # Jangan gunakan password default seperti ini di produksi
 
-        print(
-            "Inisialisasi database selesai dengan peran dan izin default serta pengguna admin."
-        )
+            if not User.query.filter_by(email=default_email).first():
+                admin_role = Role.query.filter_by(name="Admin").first()
+                admin_user = User(
+                    first_name="Xnet",
+                    last_name="Manager",
+                    email=default_email,
+                    password_hash=default_password,
+                )
+                admin_user.roles.append(admin_role)
+                db.session.add(admin_user)
+                db.session.commit()
 
+            print("Inisialisasi database selesai dengan peran dan izin default serta pengguna admin.")
+        
+        except Exception as e:
+            print(f"Error during initialization: {e}")
+            db.session.rollback()
+            raise
 
 if __name__ == "__main__":
-    initialize()
+    try:
+        initialize()
+    except Exception as e:
+        print(f"Error during initialization: {e}")
+        db.session.rollback()
--- src/models/app_models.py
+++ src/models/app_models.py
@@ -6,14 +6,12 @@ from src.config import Config
 from itsdangerous import URLSafeTimedSerializer as Serializer, SignatureExpired
 from flask import current_app
 
-
 # ------------------------------------------------------------------------
 # User and Roles Management Section
 # ------------------------------------------------------------------------
 
 
 class User(UserMixin, db.Model):
-
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
@@ -35,7 +33,34 @@ class User(UserMixin, db.Model):
     is_2fa_enabled = db.Column(db.Boolean, nullable=False, default=False)
     secret_token = db.Column(db.String, unique=True, nullable=True)
     email_verification_token = db.Column(db.String, nullable=True)
+
+    # Relasi ke Role dan Permission
     roles = db.relationship("Role", secondary="user_roles", back_populates="users")
+
+    # Relasi ke DeviceManager
+    devices = db.relationship("DeviceManager", backref="owner", lazy=True)
+
+    # Relasi ke ConfigurationManager
+    configurations = db.relationship("ConfigurationManager", backref="owner", lazy=True)
+
+    # Relasi ke BackupData
+    backups = db.relationship("BackupData", back_populates="user", lazy=True)
+
+    # Relasi ke UserBackupShare untuk mengakses backup yang dibagikan
+    backup_shares = db.relationship(
+        "UserBackupShare",
+        back_populates="user",
+        lazy=True,
+        overlaps="shared_user",
+    )
+
+    # Relasi ke ConfigurationManagerShare untuk sharing konfigurasi
+    configuration_shares = db.relationship(
+        "UserConfigurationShare",
+        back_populates="user",
+        lazy=True,
+        overlaps="shared_user",
+    )
 
     def __init__(self, first_name, last_name, email, password_hash):
         self.first_name = first_name
@@ -83,7 +108,6 @@ class User(UserMixin, db.Model):
 
 
 class Role(db.Model):
-
     __tablename__ = "roles"
 
     id = db.Column(db.Integer, primary_key=True)
@@ -148,6 +172,12 @@ class DeviceManager(db.Model):
     status = db.Column(db.String(20), nullable=True, default="Unknown")
     is_active = db.Column(db.Boolean, default=True)
 
+    # Relasi ke User
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+
+    def __repr__(self):
+        return f"<DeviceManager {self.device_name}>"
+
 
 class TemplateManager(db.Model):
     __tablename__ = "template_manager"
@@ -160,6 +190,9 @@ class TemplateManager(db.Model):
     description = db.Column(db.String(100), nullable=True)
     created_by = db.Column(db.Text, nullable=True)
 
+    def __repr__(self):
+        return f"<TemplateManager {self.template_name}>"
+
 
 class ConfigurationManager(db.Model):
     __tablename__ = "configuration_manager"
@@ -168,3 +201,100 @@ class ConfigurationManager(db.Model):
     config_name = db.Column(db.String(100), nullable=False, unique=True)
     description = db.Column(db.Text, nullable=True)
     created_by = db.Column(db.Text, nullable=True)
+
+    # Relasi ke User
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+
+    # Relasi ke UserConfigurationShare untuk sharing konfigurasi
+    shared_with = db.relationship(
+        "UserConfigurationShare", backref="configuration", lazy=True
+    )
+
+    def __repr__(self):
+        return f"<ConfigurationManager {self.config_name}>"
+
+
+class UserConfigurationShare(db.Model):
+    __tablename__ = "user_configuration_share"
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    configuration_id = db.Column(
+        db.Integer, db.ForeignKey("configuration_manager.id"), nullable=False
+    )
+
+    user = db.relationship(
+        "User",
+        back_populates="configuration_shares",
+        overlaps="configuration_shares,shared_user",
+    )
+
+    def __repr__(self):
+        return f"<UserConfigurationShare User {self.user_id} -> Configuration {self.configuration_id}>"
+
+
+# -----------------------------------------------------------------------
+# Backup Management Section
+# -----------------------------------------------------------------------
+
+
+class BackupData(db.Model):
+    __tablename__ = "backup_data"
+
+    id = db.Column(db.Integer, primary_key=True)
+    backup_name = db.Column(db.String(255), nullable=False)
+    backup_content_path = db.Column(
+        db.String(255), nullable=False
+    )  # Menyimpan path ke file backup
+    version = db.Column(db.Integer, nullable=False, default=1)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+
+    user = db.relationship(
+        "User", back_populates="backups", overlaps="shared_backups,backup_shares"
+    )
+
+    def __repr__(self):
+        return f"<BackupData {self.backup_name} v{self.version}>"
+
+    @staticmethod
+    def create_backup(backup_name, backup_content_path, user_id):
+        # Dapatkan versi terakhir dari backup dengan nama yang sama dan user yang sama
+        last_backup = (
+            BackupData.query.filter_by(backup_name=backup_name, user_id=user_id)
+            .order_by(BackupData.version.desc())
+            .first()
+        )
+        new_version = last_backup.version + 1 if last_backup else 1
+
+        new_backup = BackupData(
+            backup_name=backup_name,
+            backup_content_path=backup_content_path,
+            version=new_version,
+            user_id=user_id,
+        )
+        db.session.add(new_backup)
+        db.session.commit()
+        return new_backup
+
+
+class UserBackupShare(db.Model):
+    __tablename__ = "user_backup_share"
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    backup_id = db.Column(db.Integer, db.ForeignKey("backup_data.id"), nullable=False)
+
+    user = db.relationship(
+        "User",
+        back_populates="backup_shares",
+        overlaps="backup_shares,shared_user",
+    )
+    backup = db.relationship(
+        "BackupData",
+        backref=db.backref("shared_with", lazy=True),
+        overlaps="shared_backups",
+    )
+
+    def __repr__(self):
+        return f"<UserBackupShare User {self.user_id} -> Backup {self.backup_id}>"
